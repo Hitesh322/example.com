86f293c0d394cc0d117e3958dbc89ccd00f93037
Hitesh Mahajan <mahajanhitesh127@gmail.com>
2026-04-13 09:02:44 +0000

    added go interface program

---
+++ interface1.go
@@ -0,0 +1,36 @@
+package main
+import "fmt"
+
+type Animal interface {
+        Speak() string
+
+  
+}
+
+
+type Dog struct {}
+
+     func(d Dog) Speak() string{
+
+	     return "Bark"
+     }
+
+     type Cat struct {}
+    
+     func (c Cat) Speak() string {
+     return "Meow"
+     }
+
+   func main() {
+
+	   var a Animal
+
+	   a = Dog {}
+	   fmt.Println (a.Speak ())
+
+	   a = Cat { }
+	   fmt.Println (a.Speak())
+
+   }
+
+
