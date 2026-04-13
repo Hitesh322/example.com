0e8968c0b194c22ea1fe41626a62fa9dbbc75255
Hitesh Mahajan <mahajanhitesh127@gmail.com>
2026-04-13 08:23:54 +0000

    added go loops program

---
+++ loops.go
@@ -0,0 +1,45 @@
+package main
+import "fmt"
+
+func main() {
+	for i := 1; i <= 5 ;i++ {
+
+		fmt.Println(i)
+	}
+
+
+	    j := 1
+	    for j <=5 {
+		    fmt.Println(j)
+		    j++
+	    }
+
+	    for k := 1; k<=5; k++ {
+                if k == 3	{
+               
+                    break
+		}
+             fmt.Println(k)
+    
+	    }
+
+         for m:=1; m<=5 ; m++ {
+		 if m== 3 {
+		continue
+	}
+	fmt.Println(m)
+} 
+         count := 1
+	 for {
+		 fmt.Println("hello" )
+		 if count == 3 {
+			 break
+		 }
+		 count ++
+	 }
+      
+
+}
+
+
+   
